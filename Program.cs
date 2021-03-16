8ee68dd599e7020142e214373eb9abcb9776318d
egeserbes50 <egeserbes50@gmail.com>
2021-03-16 18:19:41 +0300

    Update

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SnakeGame
+{   
+    class Node
+    {
+       public Node next ;
+       public char value ;
+
+        public Node()
+        {
+            this.next = null ;
+            this.value = " " ;
+        }
+
+        public void changeValue(char val)
+        {
+            this.value = val ;
+        }
+
+    }
+    public struct Head 
+    {
+        Node head ;
+        int row ;
+        int coll ;
+
+    }
+
+    class GameArea 
+    {
+         public Node[,] nodeArr ;
+
+        public GameArea(int row, int coll)
+        {
+            nodeArr = new Node[row,coll];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
