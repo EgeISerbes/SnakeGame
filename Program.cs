4b8b6ccf81976974489659bfbc285b09998cdc34
egeserbes50 <egeserbes50@gmail.com>
2021-03-16 19:02:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@
 
 namespace SnakeGame
 {   
+    enum States
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+
+    }
     class Node
     {
        public Node next ;
@@ -30,12 +38,54 @@ namespace SnakeGame
     class GameArea 
     {
          public Node[,] nodeArr ;
-
-        public GameArea(int row, int coll)
+         public int row,coll ;
+        public GameArea(int row1, int coll)
         {
             nodeArr = new Node[row,coll];
+            this.row = row;
+            this.coll = coll ;
+            for (int i =0 ;i<row ; i++)
+            {
+                for(int j = 0; j<coll ; j++)
+                {
+                    nodeArr[i,j] = new Node() ;
+                }
+            }
+        }
+
+        public void showArea()
+        {
+            string roof = new string('_',this.row);
+            Console.WriteLine(roof);
+            char sides = '|';
+            string bottom = new string('-',row);
+            for(int i =0 ; i<row;i++)
+            {
+                Console.Write(sides);
+                for(int j = 0 ; j< coll ; j++)
+                {
+                    Console.Write(nodeArr[i,j].value);
+                }
+                Console.Write(sides+"\n");
+            }
+            Console.WriteLine(bottom);
         }
     }
+
+    class GameSnake : GameArea
+    {
+        public Head head ;
+        public GameSnake()
+        {
+            
+        }
+
+        public void StartGame()
+        {
+
+        }
+    }
+
 
     class Program
     {
